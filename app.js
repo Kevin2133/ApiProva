3f355729b29a0ef57a6310cd1c1c7c58264d9abe
Kevin2133 <68924818+Kevin2133@users.noreply.github.com>
2022-08-08 22:27:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    switch(req.url){
+        case "/":
+            res.writeHead(200, {"Content-Type" : "application/json"});
+            res.write(JSON.stringify({id: 0, player: "Spanoulis"}));
+            res.end();
+            break;
+        case "/prova":
+            res.writeHead(200, {"Content-Type" : "application/json"});
+            res.write(JSON.stringify("Questa e' una prova"));
+            res.end();
+            break;
+        default:
+            res.writeHead(404, "application/json");
+            res.write("Error, page does not exist");
+            res.end();
+            break;
+    }
+});
+
+server.listen(3000);
+console.log("Server listening on port 3000.....")
