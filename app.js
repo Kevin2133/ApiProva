4ec10889a6544b8f53591ff2d669b4e374735467
Kevin2133 <kevin.bellocchio@gmail.com>
2022-08-09 11:34:09 +0200

    added headers

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const data = require("./data.json");
 let port = process.env.PORT || 3000;
 
 server.get("/", (req, res) => {
+    res.header("Content-Type", "application/json");
     res.send(JSON.stringify(data));
+    res.end();
 });
 
 server.get("/manuel", (req, res) => {
+    res.header("Content-Type", "application/json");
     res.send("Bella Elia");
+    res.end();
 });
 
 server.listen(port, () => {
