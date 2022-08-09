1211cbc6d1e56cbaa41d7316e14866204245f880
Kevin2133 <kevin.bellocchio@gmail.com>
2022-08-09 13:20:14 +0200

    ok

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const server = express();
 server.use(cors({
-    origin: 'https://easycodingkev.netlify.app/'
+    origin: '*'
 }));
 const data = require("./data.json");
 
